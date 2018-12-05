c73c3389fadaa01a76eb14e8f84cdf9c3becd5a0
Mitchell Bell <45462154+mitchib1440@users.noreply.github.com>
2018-12-05 13:06:59 +0000

    Revert "Refactored code."

--- helloWorld.js
+++ helloWorld.js
@@ -2,25 +2,23 @@ var finalScore = 73;
 var highScore = 78;
 var levelCompleted = true;
 
-const logHighScore = () => {
-	return "New high score! You got " + highScore + " points!";
+const testHighScore = (finalScore, highScore) => {
+	if(finalScore >= highScore) {
+        return true;
+	}
 };
 
 const endGame = () => {
 	return "Game over!";
 };
 
-
-const testHighScore = (finalScore, highScore) => {
-	if(finalScore >= highScore) {
-        return true;
-	logHighScore()
-	}
+const logHighScore = () => {
+	return "New high score! You got " + highScore + " points!";
 };
 
 if(levelCompleted) {
     if (testHighScore(finalScore, highScore) === true) {
-        console.log(logHighScore());
+        logHighScore();
     }
 	endGame();
 }
